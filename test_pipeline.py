3dad961d618f4e4ee23b844b0745b8550f90de6a
Harshan07-web <bharshan17@gmail.com>
2025-12-30 14:30:52 +0530

    commit

--- test_pipeline.py
+++ test_pipeline.py
@@ -6,8 +6,8 @@ pipeline = Pipeline()
 print("=== Solve ===")
 
 # Change expression here for testing:
-user_expression = "abc"
-#user_expression = "3*(x-1)=15 "   # → Will ask: differentiate or integrate?
+#user_expression = "abc"
+user_expression = "2*x^2 + 4*x +4 = 0 "   # → Will ask: differentiate or integrate?
 #user_expression = "∫(x^2 + 2) dx"  # If you later support symbolic integral frontend
 
 result = pipeline.solve_and_explain(user_expression)
